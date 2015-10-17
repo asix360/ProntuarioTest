d50c75724829e3092a853713c9adb244db3798fb
asix360 <alessandro_eddy@hotmail.com>
2015-10-17 07:26:26 +0000

    adicionado a obrigacao de esta logado para acessar o site

---
+++ atendimentos/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from .form import PacienteForm
+
+
+def poslogin(request):
+
+    if request.user.id:
+     return render(request, 'Bem_vindo.html', {})
+    else:
+        return HttpResponseRedirect("/")
+
+
+def cadastroPaciente(request):
+    # if this is a POST request we need to process the form data
+    if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = PacienteForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            form.save()
+            # redirect to a new URL:
+            return HttpResponseRedirect('/inicio/')
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = PacienteForm()
+
+    return render(request, 'CadastroPaciente.html', {'form': form})
